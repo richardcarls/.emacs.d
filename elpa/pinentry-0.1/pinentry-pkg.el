a60ccec8b0bfa2bd55c8cfaf930db529e36366f0
Richard Carls <richard.j.carls@gmail.com>
2017-09-13 22:40:34 -0400

    Remove color customization

--- elpa/pinentry-0.1/pinentry-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "pinentry" "0.1" "GnuPG Pinentry server implementation" 'nil :url "http://elpa.gnu.org/packages/pinentry.html" :keywords '("gnupg"))
--- init.el
+++ init.el
@@ -63,21 +63,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
  '(before-save-hook (quote (whitespace-cleanup)))
  '(blink-cursor-mode t)
  '(compilation-message-face (quote default))
  '(css-indent-offset 2)
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-normal-cursor-color "#839496")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
  '(custom-enabled-themes nil)
- '(custom-safe-themes
-   (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(fci-rule-color "#073642")
  '(flycheck-checkers
    (quote
     (ada-gnat asciidoc c/c++-clang c/c++-gcc c/c++-cppcheck cfengine chef-foodcritic coffee coffee-coffeelint coq css-csslint d-dmd dockerfile-hadolint elixir-dogma emacs-lisp emacs-lisp-checkdoc erlang eruby-erubis fortran-gfortran go-gofmt go-golint go-vet go-build go-test go-errcheck go-unconvert groovy haml handlebars haskell-stack-ghc haskell-ghc haskell-hlint html-tidy javascript-standard javascript-eslint javascript-jshint javascript-gjslint javascript-jscs json-jsonlint json-python-json less lua-luacheck lua perl perl-perlcritic php php-phpmd php-phpcs processing protobuf-protoc pug puppet-parser puppet-lint python-flake8 python-pylint python-pycompile r-lintr racket rpm-rpmlint markdown-mdl rst-sphinx rst ruby-rubocop ruby-rubylint ruby ruby-jruby rust-cargo rust scala scala-scalastyle scheme-chicken scss-lint sass/scss-sass-lint sass scss sh-bash sh-posix-dash sh-posix-bash sh-zsh sh-shellcheck slim slim-lint sql-sqlint systemd-analyze tex-chktex tex-lacheck texinfo typescript-tslint verilog-verilator xml-xmlstarlet xml-xmllint yaml-jsyaml yaml-ruby)))
@@ -86,29 +76,6 @@
  '(flycheck-global-modes t)
  '(global-auto-complete-mode t)
  '(global-flycheck-mode t)
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#002b36" 0.25)
-    (quote
-     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
- '(highlight-symbol-foreground-color "#93a1a1")
- '(highlight-tail-colors
-   (quote
-    (("#073642" . 0)
-     ("#546E00" . 20)
-     ("#00736F" . 30)
-     ("#00629D" . 50)
-     ("#7B6000" . 60)
-     ("#8B2C02" . 70)
-     ("#93115C" . 85)
-     ("#073642" . 100))))
- '(hl-bg-colors
-   (quote
-    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
- '(hl-fg-colors
-   (quote
-    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(initial-major-mode (quote projectile-global-mode))
@@ -118,40 +85,13 @@
  '(js2-mode-show-parse-errors nil)
  '(js2-mode-show-strict-warnings nil)
  '(magit-diff-use-overlays nil)
- '(nrepl-message-colors
-   (quote
-    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
  '(package-selected-packages
    (quote
-    (flycheck expand-region color-theme-modern color-theme-approximate scss-mode dockerfile-mode php-mode ac-php json-reformat web-mode solarized-theme tern tern-auto-complete jinja2-mode js2-mode markdown-mode mustache-mode multi-web-mode flycheck-tip autumn-light-theme color-theme hydandata-light-theme frame-restore multi-term syslog-mode flx-ido itail yaml-mode pastelmac-theme neotree magit projectile jsx-mode json-mode js-doc flyparens flymake-yaml flymake-sass flymake-json flymake-jslint flymake-jshint flymake-css ac-js2 ac-html)))
- '(pos-tip-background-color "#073642")
- '(pos-tip-foreground-color "#93a1a1")
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
+    (dracula-theme flycheck expand-region color-theme-modern color-theme-approximate scss-mode dockerfile-mode php-mode ac-php json-reformat web-mode solarized-theme tern tern-auto-complete jinja2-mode js2-mode markdown-mode mustache-mode multi-web-mode flycheck-tip autumn-light-theme color-theme hydandata-light-theme frame-restore multi-term syslog-mode flx-ido itail yaml-mode pastelmac-theme neotree magit projectile jsx-mode json-mode js-doc flyparens flymake-yaml flymake-sass flymake-json flymake-jslint flymake-jshint flymake-css ac-js2 ac-html)))
  '(standard-indent 2)
  '(term-default-bg-color nil)
- '(term-default-fg-color "#839496")
  '(vc-annotate-background nil)
  '(vc-annotate-background-mode nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#dc322f")
-     (40 . "#c37300")
-     (60 . "#b97d00")
-     (80 . "#b58900")
-     (100 . "#a18700")
-     (120 . "#9b8700")
-     (140 . "#948700")
-     (160 . "#8d8700")
-     (180 . "#859900")
-     (200 . "#5a942c")
-     (220 . "#439b43")
-     (240 . "#2da159")
-     (260 . "#16a870")
-     (280 . "#2aa198")
-     (300 . "#009fa7")
-     (320 . "#0097b7")
-     (340 . "#008fc7")
-     (360 . "#268bd2"))))
  '(vc-annotate-very-old-color nil)
  '(web-mode-auto-close-style 1)
  '(web-mode-code-indent-offset 2)
@@ -164,21 +104,15 @@
  '(web-mode-enable-current-element-highlight t)
  '(web-mode-markup-indent-offset 2)
  '(web-mode-script-padding 2)
- '(web-mode-style-padding 2)
- '(weechat-color-list
-   (quote
-    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
- '(xterm-color-names
-   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
- '(xterm-color-names-bright
-   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
+ '(web-mode-style-padding 2))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(term ((t (:inherit default :background "black" :foreground "#839496")))))
+ '(term ((t (:inherit default :background "black" :foreground "#839496"))))
+ '(web-mode-html-tag-bracket-face ((t nil))))
 
 (provide 'init)
 ;;; init.el ends here
